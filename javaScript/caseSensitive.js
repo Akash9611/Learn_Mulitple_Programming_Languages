1f0b27b3a28b9dccfd78811a36f60367fb6520a3
Akash <gpkp176172@gmail.com>
2025-11-08 15:50:10 +0530

    JS -> JavaScript is CaseSensitive, variables are considered different due to case sensitivity (ex name, Name and NAME)

---
+++ javaScript/caseSensitive.js
@@ -0,0 +1,9 @@
+// JavaScript is case-sensitive. The following variables are considered different due to case sensitivity:
+
+let name = "Akash";
+let Name = "Akash";
+let NAME = "Akash";
+
+console.log(name);
+console.log(Name);
+console.log(NAME);
