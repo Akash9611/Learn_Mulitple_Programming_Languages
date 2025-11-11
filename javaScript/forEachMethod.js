e7ba5503b2d730a8079bf181549a83e8e5b465bd
Akash <gpkp176172@gmail.com>
2025-11-11 23:56:39 +0530

    JS- what is HIGHER ORDER Function/Methods

--- javaScript/forEachMethod.js
+++ javaScript/forEachMethod.js
@@ -1,8 +1,8 @@
 // forEach - is an in-built array method in JavaScript that executes a provided function once for each array element.
 // It is a callback function that is called for every element in the array.
 
-// Syntax: array.forEach(function(currentValue, index, array){}, thisValue)
-//Syntax with arrow function: array.forEach((currentValue, index, array) => {}, thisValue)
+// Syntax: array.forEach(function(currentValue, index, array){})
+//Syntax with arrow function: array.forEach((currentValue, index, array) => {})
 
 // Example 1: Using forEach to print each element of an array
 let fruits = ['Apple', 'Banana', 'Mango', 'Orange'];
@@ -22,3 +22,13 @@ console.log(`Sum of all numbers is: ${sum}`);
 // Interview Level Question:
 // Q. What is higher order function or method in JavaScript?
 // -> Higher order function- are the javascript function that can take another function as an argument or return a function as a result.
+
+
+//Example- For given array find square of each number using forEach method and higher order function concept.
+let arr = [1, 2, 3, 4, 5];
+
+const squareOfNumbers = (num) => {
+    console.log(num * num); // num**2
+}
+
+arr.forEach(squareOfNumbers); // passing function as argument to forEach method
