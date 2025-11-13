c06f5ae04686e337202e63fc8f6feafda96176bf
Akash <gpkp176172@gmail.com>
2025-11-13 09:21:10 +0530

    Java -> String Palindrome [by usign reverse string and comparing]

--- java/stringPalindrome.java
+++ java/stringPalindrome.java
@@ -2,6 +2,7 @@
 // Palindrome: A string that reads the same backward as forward
 
 public class stringPalindrome {
+    // Method 1 - to check palindrome
     public static boolean isPalindrome(String str) {
         int left = 0;
         int right = str.length() - 1;
@@ -15,19 +16,38 @@ public class stringPalindrome {
 
         // using for loop
         // for (int i = 0, j = str.length() - 1; i < j; i++, j--) {
-        //     if (str.charAt(i) != str.charAt(j)) {
-        //         return false;
-        //     }
+        // if (str.charAt(i) != str.charAt(j)) {
+        // return false;
+        // }
         // }
         return true;
     }
 
+    // Method 2- to reverse a string and check palindrome
+    public static void reverseString() {
+        System.out.println("--- Starting reverseString method ---");
+        String str = "madam";
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+        System.out.println("Reversed String: " + reversed);
+
+        if (str.equals(reversed)) {
+            System.out.println(str + " is a palindrome.----");
+        } else {
+            System.out.println(str + " is not a palindrome.----");
+        }
+    }
+
     public static void main(String[] args) {
         String testStr = "radar";
+        reverseString();
         if (isPalindrome(testStr)) {
             System.out.println(testStr + " is a palindrome.");
         } else {
             System.out.println(testStr + " is not a palindrome.");
         }
+
     }
 }
